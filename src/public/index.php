57555271544cf56cf4bdcfc402a1caabf94215e6
Bassil-ali <githup7@gmail.com>
2021-08-12 20:23:09 +0200

    edit

--- src/public/index.php
+++ src/public/index.php
@@ -5,3 +5,10 @@ echo random_int(1,200);
 echo "\n <br>";
 
 echo hash('ripemd160', 'The quick brown fox jumped over the lazy dog.');
+
+$data = ['bassil' , 'ali' , 'ahmed'];
+
+foreach ($data as  $value) {
+    
+    echo "<br>". $value . " greater";
+}
